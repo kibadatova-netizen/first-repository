b80e36cb5a633e219f5e63e68c0a0aaf9d996c8f
kibadatova-netizen <kibadatova@gmail.com>
2025-11-10 20:27:01 -0600

    second commit finish

--- src/Practice.java
+++ src/Practice.java
@@ -1,5 +1,6 @@
 public class Practice {
     public static void main(String[] args) {
         System.out.println("Hello, this is my second commit");
+        System.out.println("bye, my second commit is finish");
     }
 }
