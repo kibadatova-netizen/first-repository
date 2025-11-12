5832ae41c837112194be18f77ef0b9ec76d24383
kibadatova-netizen <kibadatova@gmail.com>
2025-11-11 18:30:17 -0600

    already created

--- src/day_1_first_some_logic/CreateBranch.java
+++ src/day_1_first_some_logic/CreateBranch.java
@@ -4,6 +4,7 @@ public class CreateBranch {
     public static void main(String[] args) {
         for (int i = 0; i < 10; i++) {
             System.out.println("Hello, I am creating a new branch");
+            System.out.println("Bye, I created a new branch");
         }
     }
 }
